ea5b5285337937154ba2c2d642821d15ac3b83cb
jkebx22 <42923140+jkebx22@users.noreply.github.com>
2019-02-09 11:08:32 -0600

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MovieStorage
+{
+    public class Movie
+    {
+
+        public string title;
+        public string director;
+        public string year;
+
+        public Movie() { }
+
+        public Movie(string theTitle, string theDirector, string theYear)
+        {
+
+            theTitle = title;
+            theDirector = director;
+            theYear = year;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { Title = title; }
+        }
+
+        public string Director
+        {
+            get { return director; }
+            set { Director = director; }
+        }
+
+        public string Year
+        {
+            get { return year; }
+            set { Year = year; }
+        }
+
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MovieStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movie TommyBoy = new Movie("Tommy Boy","Peter Segal", "1995" );
+            Movie BlackSheep = new Movie("Black Sheep", "Penelope Spheeris", "1996");
+
+            TommyBoy.title = "Tommy Boy";
+            TommyBoy.director = "Peter Segal";
+            TommyBoy.year = "1995";
+
+            BlackSheep.title = "Black Sheep";
+            BlackSheep.director = "Penelope Spheers";
+            BlackSheep.year = "1996";
+
+            Console.WriteLine(TommyBoy.title + TommyBoy.director + " " + TommyBoy.year + ", " + BlackSheep.title + BlackSheep.director + " " + BlackSheep.year);
+
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
